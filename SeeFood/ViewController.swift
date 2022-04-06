c57bc6d84e3fcef8bea09a50e5577bcfdbbe8b72
Karen Turner <krt@karenrenee.com>
2022-04-06 11:12:37 -0600

    Complete through lesson 325 - add Image recognition methods

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -24,12 +26,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userChosenImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userChosenImage
+            
+            guard let ciImage = CIImage(image: userChosenImage) else {
+                fatalError("Error converting image to CIImage")
+            }
+            
+            detect(image: ciImage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as?[VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+        try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
 
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
     }
     
 }
