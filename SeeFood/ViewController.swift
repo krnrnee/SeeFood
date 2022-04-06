b9fbe274088c8f4537555c87319c6dfe4240d07a
Karen Turner <krt@karenrenee.com>
2022-04-06 11:30:08 -0600

    Complete project

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -47,6 +47,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 fatalError("Model failed to process image")
             }
             print(results)
+            
+             if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog"
+                }
+                else {
+                    self.navigationItem.title = "Not Hotdog"
+                    self.navigationItem.title = firstResult.identifier
+                }
+            
+            }
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
